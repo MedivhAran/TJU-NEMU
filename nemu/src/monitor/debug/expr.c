aac1779b9b77024d8c7153107d6927e29bf8d8e5
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-11 05:33:57 +0000

    >  compile NEMU 3023206100 medivh Linux 87b834790a92 6.14.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug  7 18:32:38 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  05:33:57 up  1:58,  0 users,  load average: 0.55, 0.30, 0.22 c96ee36e582275da09dd9b24e97080bf143279e

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -1,5 +1,4 @@
 #include "nemu.h"
-
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
  */
@@ -67,6 +66,7 @@ typedef struct token {
 Token tokens[32];
 int nr_token;
 
+
 static bool make_token(char *e) {
 	int position = 0;
 	int i;
@@ -111,16 +111,167 @@ static bool make_token(char *e) {
 	return true;
 }
 
+
+static uint32_t my_str2reg(const char *s, bool *success) {
+    // s 是寄存器名
+    int i;
+    for (i = 0; i < 8; i++) {
+        if (strcmp(s, regsl[i]) == 0) { // regsl[] 存放寄存器名字，定义在 reg.c
+            *success = true;
+            return reg_l(i);            // reg_l(i) 获取 32位寄存器值
+        }
+    }
+
+	for (i = 0; i < 8; i++) {
+        if (strcmp(s, regsw[i]) == 0) { // regsl[] 存放寄存器名字，定义在 reg.c
+            *success = true;
+            return reg_w(i);            // reg_l(i) 获取 32位寄存器值
+        }
+    }
+
+	for (i = 0; i < 8; i++) {
+        if (strcmp(s, regsb[i]) == 0) { // regsl[] 存放寄存器名字，定义在 reg.c
+            *success = true;
+            return reg_b(i);            // reg_l(i) 获取 32位寄存器值
+        }
+    }
+
+
+    if (strcmp(s, "eip") == 0) {
+        *success = true;
+        return cpu.eip;
+    }
+
+    // 找不到寄存器
+    *success = false;
+    printf("Unknown register: %s\n", s);
+    return 0;
+}
+
+
+static bool check_parentheses(int p, int q){
+	if (tokens[p].type != '(' || tokens[q].type != ')') {
+        return false;
+    }
+	char stack[32];
+	int top = -1;
+
+	int i = p;
+
+	for (i = p; i <= q; i++) {
+		if (tokens[i].type == '(') {
+			stack[++top] = '(';
+		} else if (tokens[i].type == ')') {
+			if (top == -1) {
+				return false; // 右括号多余
+			}
+			top--; // 弹出栈顶元素
+		}
+	}
+	return top == -1; // 栈空表示括号匹配
+}
+
+static int precedence(int type) {
+    switch (type) {
+        case OR:  return 1;
+        case AND: return 2;
+        case EQ: case NEQ: return 3;
+        case '+': return 4;
+		case '-': return 4;
+        case '*': return 5;
+		case '/': return 5;
+    }
+    return 0;
+}
+
+static int dominant_op(int p, int q) {
+    int op = -1;
+    int min_pri = 100;
+    int balance = 0;
+	int i;
+    for (i = p; i <= q; i++) {
+        if (tokens[i].type == '(') { balance++; continue; }
+        if (tokens[i].type == ')') { balance--; continue; }
+        if (balance > 0) continue; // 括号中内容跳过
+
+        int pri = precedence(tokens[i].type);
+        if (pri > 0 && pri <= min_pri) {
+            min_pri = pri;
+            op = i;
+        }
+    }
+    return op;
+}
+
+static int eval(int p, int q) {
+	if (p > q) {
+		return 0;
+	}
+
+	else if (p == q) {
+		int val = 0;
+		if (tokens[p].type == DEC) {
+			sscanf(tokens[p].str, "%d", &val);
+			return val;
+		} else if (tokens[p].type == HEX) {
+			sscanf(tokens[p].str, "%x", &val);
+			return val;
+		} else if (tokens[p].type == REG) {
+			bool ok = true;
+            return my_str2reg(tokens[p].str + 1, &ok); // 跳过 $
+		} else {
+			Assert(0, "Unexpected token type %d", tokens[p].type);
+			return 0;
+		}
+	}
+
+	else if (check_parentheses(p, q) == true) {
+		return eval(p + 1, q - 1);
+	}
+	else {		//没有括号的情况
+		int op = dominant_op(p, q);
+		if (op == -1) {
+			return 0;
+		}
+
+		int val1 = eval(p, op - 1);
+
+		int val2 = eval(op + 1, q);
+
+		switch (tokens[op].type) {
+			case '+': return val1 + val2;
+			case '-': return val1 - val2;
+			case '*': return val1 * val2;
+			case '/': 
+				if (val2 == 0) {
+					printf("Error: Cannot Divide zero!~\n");
+					return 0;
+				}
+				return val1 / val2;
+			case EQ: return val1 == val2;
+			case NEQ: return val1 != val2;
+			case AND: return val1 && val2;
+			case OR: return val1 || val2;
+			default:
+				return 0;
+		}
+	}
+}
+
+
+
+
+
+
+
 uint32_t expr(char *e, bool *success) {
 	if(!make_token(e)) {
 		*success = false;
 		return 0;
 	}
-
+	*success = true;
 	/* TODO: Insert codes to evaluate the expression. */
-	panic("please implement me");
-	return 0;
+	return eval(0, nr_token - 1);
 }
-
 
 
