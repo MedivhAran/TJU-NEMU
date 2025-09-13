9a76b140887ed5892f3c269533977fde6f2af80d
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-13 15:09:29 +0000

    >  compile NEMU 3023206100 medivh Linux 87b834790a92 6.14.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug  7 18:32:38 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:09:29 up  1:09,  0 users,  load average: 0.21, 0.20, 0.21 48410a493dcc226b349c2a5577839ecf970f5f5f

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -1,75 +1,67 @@
+#include "common.h"
 #include "monitor/monitor.h"
 #include "monitor/expr.h"
 #include "monitor/watchpoint.h"
 #include "nemu.h"
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
 void cpu_exec(uint32_t);
 
-
-/* We use the `readline' library to provide more flexibility to read from stdin. */
-char* rl_gets() {
-	static char *line_read = NULL;
-
-	if (line_read) {
-		free(line_read);
-		line_read = NULL;
-	}
-
-	line_read = readline("(nemu) ");
-
-	if (line_read && *line_read) {
-		add_history(line_read);
-	}
-
-	return line_read;
+static char* rl_gets(void) {
+    static char *line_read = NULL;
+    if (line_read) {
+        free(line_read);
+        line_read = NULL;
+    }
+    line_read = readline("(nemu) ");
+    if (line_read && *line_read) {
+        add_history(line_read);
+    }
+    return line_read;
 }
 
-
-
-
-//六个指令
 static int cmd_c(char *args) {
-	cpu_exec(-1);
-	return 0;
+    cpu_exec(-1);
+    return 0;
 }
+
 static int cmd_q(char *args) {
-	return -1;
+    return -1;
 }
 
 static int cmd_help(char *args);
-
-//单步调试
 static int cmd_si(char *args);
-//打印寄存器信息
 static int cmd_info(char *args);
-//扫描内存
 static int cmd_scan(char *args);
-
 static int cmd_eval(char *args);
+static int cmd_w(char *args);
+static int cmd_d(char *args);
 
 static struct {
-	char *name;
-	char *description;
-	int (*handler) (char *);	//函数指针
-	} cmd_table [] = {
-	{ "help", "Display informations about all supported commands", cmd_help },
-	{ "c", "Continue the execution of the program", cmd_c },
-	{ "q", "Exit NEMU", cmd_q },
-	/* TODO: Add more commands */
-	{ "si", "step into (n steps)", cmd_si},
-	{"info", "show info of register", cmd_info},
-	{"x", "scan memory", cmd_scan},
-	{"p", "evaluate", cmd_eval},
+    char *name;
+    char *description;
+    int (*handler)(char *);
+} cmd_table [] = {
+    { "help", "Display informations about all supported commands", cmd_help },
+    { "c", "Continue the execution of the program", cmd_c },
+    { "q", "Exit NEMU", cmd_q },
+    { "si", "Step execute N instructions (default 1)", cmd_si },
+    { "info", "Show registers or watchpoints", cmd_info },
+    { "x", "Scan memory", cmd_scan },
+    { "p", "Evaluate expression", cmd_eval },
+    { "w", "Set watchpoint", cmd_w },
+    { "d", "Delete watchpoint", cmd_d },
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
 
-static int cmd_si(char *args){
-	int step = 1;
+static int cmd_si(char *args) {
+    int step = 1;
     if (args != NULL) {
         sscanf(args, "%d", &step);
     }
@@ -78,29 +70,36 @@ static int cmd_si(char *args){
 }
 
 static int cmd_info(char *args) {
+    int i;
     if (args == NULL) return 0;
-	//若用户输入的参数为 r
+
     if (strcmp(args, "r") == 0) {
-        {
-			int i;
-			//依次打印所有寄存器的信息
-    		for (i = 0; i < 8; i++) {
-			//寄存器名字 寄存器16进制的值  寄存器10进制的值
-        	printf("%s\t0x%08x\t%d\n", regsl[i], reg_l(i), reg_l(i));
-    	}
-    	printf("eip\t0x%08x\t%d\n", cpu.eip, cpu.eip);
-		}
-    } 
+        for (i = 0; i < 8; i++) {
+            printf("%s\t0x%08x\t%d\n", regsl[i], reg_l(i), reg_l(i));
+        }
+        printf("eip\t0x%08x\t%d\n", cpu.eip, cpu.eip);
+    }
+    else if (strcmp(args, "w") == 0) {
+        WP *wp = head;
+        if (!wp) {
+            printf("No watchpoints.\n");
+        } else {
+            printf("Num\tExpr\t\tValue\n");
+            while (wp) {
+                printf("%d\t%s\t%u (0x%x)\n", wp->NO, wp->expr, wp->value, wp->value);
+                wp = wp->next;
+            }
+        }
+    }
     return 0;
 }
 
 static int cmd_scan(char *args) {
     int N;
     uint32_t addr;
-    if (args == NULL) 
-	{return 0;}
+    int i;
+    if (args == NULL) return 0;
     sscanf(args, "%d %x", &N, &addr);
-	int i = 0;
     for (i = 0; i < N; i++) {
         uint32_t data = swaddr_read(addr + i * 4, 4);
         printf("0x%08x: 0x%08x\n", addr + i * 4, data);
@@ -108,30 +107,25 @@ static int cmd_scan(char *args) {
     return 0;
 }
 
-
-
-
 static int cmd_help(char *args) {
-	/* extract the first argument */
-	char *arg = strtok(NULL, " ");
-	int i;
-
-	if(arg == NULL) {
-		/* no argument given */
-		for(i = 0; i < NR_CMD; i ++) {
-			printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-		}
-	}
-	else {
-		for(i = 0; i < NR_CMD; i ++) {
-			if(strcmp(arg, cmd_table[i].name) == 0) {
-				printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-				return 0;
-			}
-		}
-		printf("Unknown command '%s'\n", arg);
-	}
-	return 0;
+    char *arg = strtok(NULL, " ");
+    int i;
+    if (arg == NULL) {
+        for (i = 0; i < NR_CMD; i++) {
+            printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        }
+    } else {
+        for (i = 0; i < NR_CMD; i++) {
+            if (strcmp(arg, cmd_table[i].name) == 0) {
+                printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+                break;
+            }
+        }
+        if (i == NR_CMD) {
+            printf("Unknown command '%s'\n", arg);
+        }
+    }
+    return 0;
 }
 
 static int cmd_eval(char *args) {
@@ -146,44 +140,49 @@ static int cmd_eval(char *args) {
 	return 0;
 }
 
-
-void ui_mainloop() {
-	while(1) {
-		//从终端读取一行用户输入
-		char *str = rl_gets();
-		char *str_end = str + strlen(str);
-
-		/* extract the first token as the command */
-		char *cmd = strtok(str, " ");
-		if(cmd == NULL) { continue; }
-
-		/* treat the remaining string as the arguments,
-		 * which may need further parsing
-		 */
-		//cmd此时是指令的本体，现在需要分割后面的参数，+1是因为空格
-		char *args = cmd + strlen(cmd) + 1;
-		if(args >= str_end) {
-			args = NULL;
-		}
-
-#ifdef HAS_DEVICE
-		extern void sdl_clear_event_queue(void);
-		sdl_clear_event_queue();
-#endif
-
-		int i;
-		//遍历指令
-		for(i = 0; i < NR_CMD; i ++) {
-			if(strcmp(cmd, cmd_table[i].name) == 0) {
-				if(cmd_table[i].handler(args) < 0) { return; }
-				break;
-			}
-		}
-
-		if(i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
-	}
+static int cmd_w(char *args) {
+    if (args == NULL) return 0;
+    new_wp(args);
+    return 0;
 }
 
+static int cmd_d(char *args) {
+    int no;
+    if (args == NULL) return 0;
+    no = atoi(args);
+    free_wp(no);
+    return 0;
+}
 
+void ui_mainloop(void) {
+    while (1) {
+        char *str = rl_gets();
+        char *str_end = str + strlen(str);
 
+        char *cmd = strtok(str, " ");
+        if (cmd == NULL) { continue; }
 
+        char *args = cmd + strlen(cmd) + 1;
+        if (args >= str_end) {
+            args = NULL;
+        }
+
+#ifdef HAS_DEVICE
+        extern void sdl_clear_event_queue(void);
+        sdl_clear_event_queue();
+#endif
+
+        {
+            int i;
+            for (i = 0; i < NR_CMD; i++) {
+                if (strcmp(cmd, cmd_table[i].name) == 0) {
+                    if (cmd_table[i].handler(args) < 0) { return; }
+                    break;
+                }
+            }
+            if (i == NR_CMD) {
+                printf("Unknown command '%s'\n", cmd);
+            }
+        }
+    }
+}
