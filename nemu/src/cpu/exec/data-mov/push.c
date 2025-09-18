188b9310944b6839095c7e40d1bd00f2d2f7297c
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-18 15:06:57 +0000

    >  compile NEMU 3023206100 medivh Linux 87b834790a92 6.14.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug  7 18:32:38 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:06:57 up  4:25,  0 users,  load average: 0.34, 0.33, 0.27 599dada9c5072dfe9b924e20a692947696728048

--- nemu/src/cpu/exec/data-mov/push.c
+++ nemu/src/cpu/exec/data-mov/push.c
@@ -1,17 +1,19 @@
-// 文件: nemu/src/cpu/exec/data-mov/push.c
-
 #include "cpu/exec/helper.h"
-
 // 'r' 表示寄存器, 'v' 表示 32位 (在 PA2 中)
 make_helper(push_r_v) {
-
+    // 解码操作数：从 opcode 中获取寄存器编号
+    // 0x50 是 push %eax, 0x51 是 push %ecx ...
+    // 所以寄存器编号就是 opcode 的最后 3 个 bit
     int reg_code = instr_fetch(eip, 1) & 0x7;
 
     // 执行 push 操作
     cpu.esp -= 4;
+    // cpu.gpr[reg_code]._32 就是对应的32位寄存器
     swaddr_write(cpu.esp, 4, cpu.gpr[reg_code]._32);
 
-    // 打印调试信息
+    // 打印调试信息 (非常重要! 之前你漏了这一行)
+    // reg_name() 是框架提供的函数，根据编号获取寄存器名字
+    print_asm("push ");
     // 返回指令长度
     return 1;
 }
