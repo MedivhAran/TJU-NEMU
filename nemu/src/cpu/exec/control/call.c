9e80df675c49ca5c7f92d1c2427da58c839839e8
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-24 12:10:50 +0000

    >  compile NEMU 3023206100 medivh Linux 87b834790a92 6.14.0-29-generic #29-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug  7 18:32:38 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:10:50 up  3:09,  0 users,  load average: 0.66, 0.66, 0.62 a855982a825e756aca9aafb93694c33a6b3582b3

--- nemu/src/cpu/exec/control/call.c
+++ nemu/src/cpu/exec/control/call.c
@@ -1,20 +1,20 @@
 #include "cpu/exec/helper.h"
  
-// 定义DATA_BYTE为1，生成处理1字节的数据的call指令函数
+
 #define DATA_BYTE 1
 #include "call-template.h"
 #undef DATA_BYTE
  
-// 定义DATA_BYTE为2，生成处理2字节的数据的call指令函数
+
 #define DATA_BYTE 2
 #include "call-template.h"
 #undef DATA_BYTE
  
-// 定义DATA_BYTE为4，生成处理4字节的数据的call指令函数
+
 #define DATA_BYTE 4
 #include "call-template.h"
 #undef DATA_BYTE
  
-//定义重载函数_v来处理不同数据长度的指令
+
 make_helper_v(call_i)
 make_helper_v(call_rm)
